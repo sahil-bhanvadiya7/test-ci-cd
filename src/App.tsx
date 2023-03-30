45d2445f12d08cfa9613179462d4dd3aa7dace1d
sahil-bhanvadiya7 <sahil.bhanvadiya@seaflux.tech>
2023-03-30 13:16:16 +0530

    feat: test cicd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import githubLogo from '../public/github.svg'
+import cicdLogo from '../public/cicd.svg'
 import './App.css'
 
 function App() {
@@ -9,21 +11,24 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a target="_blank">
+          <img src={githubLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a target="_blank">
+          <img src={cicdLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Github + CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
